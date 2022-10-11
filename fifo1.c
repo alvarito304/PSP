66b03a18b6e6c830cbdec3cfe08e83b245c95d60
alvarito304 <114983881+alvarito304@users.noreply.github.com>
2022-10-11 10:41:34 +0200

    Add files via upload

---
+++ fifo1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+int main(void)
+{
+  int  fp;
+  int  p,  bytesleidos;
+  char buffer[10];
+
+   p=mkfifo("FIFO2",  S_IFIFO|0666);//permiso  de  lecture y escritura y creamos mkfifo("su nombre",tipo de dato del fifo|los permisos)
+	
+  while (1)
+  {
+	fp = open("FIFO2", 0);//abrimos el fifo de escritura por eso el 0
+	bytesleidos=read(fp,buffer,1) ; //almacenamos el primer caracter de fp lo mandamos al buffer solo 1 caracter
+	printf("OBTENIENDO  Informacion...");
+	
+	if (isdigit(buffer[0])){	
+	  printf("SOY UN NUMERO\n");
+	while 	(bytesleidos!=0)//creamos un bucle para leer el archivo
+	{
+	   printf("%c",buffer[0]); //printeamos el buffer [0]
+	   bytesleidos=read(fp,buffer,1)	;//leo  otro  byte
+	}
+	close(fp); 
+	}else{
+	printf("SOY UNA LETRA\n");
+	while 	(bytesleidos!=0)//creamos un bucle para leer el archivo
+	{
+	   printf("%c",buffer[0]); //printeamos el buffer [0]
+	   bytesleidos=read(fp,buffer,1)	;//leo  otro  byte
+	}
+	close(fp);
+	}
+  }  
+return(0); 
+}
+	
---
+++ fifoletra.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+int  main()
+{
+   int  fp;
+   char  saludo[]= "Un  saludo!!!\n";
+	
+    fp = open("FIFO2",1);//abrimos el fifo de escritura por eso el "1"
+
+    printf("Mandando  informacion  al  FIFO...\n"); 
+    write(fp,saludo,strlen(saludo));//exribimos en el fifo(archivo)(fpque es el fifo que hemos abierto,var "saludo",strlen(saludo))
+   close(fp);
+   return 0;
+}
